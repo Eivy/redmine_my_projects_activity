8aafda0b8d6b99782df422fded76ddab72ac27a3
Eivy <modern.times.rock.and.roll+git@gmail.com>
2016-03-07 14:18:47 +0900

    Bump version to 0.0.2

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :redmine_my_projects_activity do
   name 'Redmine My Projects Activity plugin'
   author 'Eivy'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/Eivy/redmine_my_projects_activity'
   author_url 'https://github.com/Eivy/'
 end
